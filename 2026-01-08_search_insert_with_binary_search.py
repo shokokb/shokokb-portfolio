70ae46204a20dd8f977c333e0a33d7d47d325409
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-01-10 09:00:10 +0900

    Fix Search insert solution (2026-10-08)

--- 2026-01-08_search_insert_with_binary_search.py
+++ 2026-01-08_search_insert_with_binary_search.py
@@ -2,10 +2,10 @@
 # Space Complexity : O(1)
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)
-        while l < r:
+        l, r = 0, len(nums) - 1  # 右端は最後の index
+        while l <= r:
             mid = (l + r) // 2
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 l = mid + 1
