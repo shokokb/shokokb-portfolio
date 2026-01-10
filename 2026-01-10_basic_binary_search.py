91e4bf16f63a18617a1feff75fd0fc36ffe853a0
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2026-01-10 09:09:56 +0900

    Add Binary search solution(2026-01-10)

---
+++ 2026-01-10_basic_binary_search.py
@@ -0,0 +1,14 @@
+# Time Complexity : O(log n)
+# Space Complexity : O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1 
+        return -1
+        
